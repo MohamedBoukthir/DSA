dacdbcdbe8b81f70131445309584234254d8e0e1
MohamedBoukthir <medboukthir7@gmail.com>
2024-06-12 11:00:06 +0100

    array intro

---
+++ src/array/Array.java
@@ -0,0 +1,15 @@
+package array;
+
+/*
+    - it"s a collection of data elements of specified type.
+    - all data holding partitions have contiguous memory location.
+    - each partition has two neighbors except first and last one.
+    - size of array is fixed and cannot be modified once it s created.
+    - being adjacent each partition is indexed and can be determined by its position.
+*/
+
+public class Array {
+    public static void main(String[] args) {
+
+    }
+}
